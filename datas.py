79c1d45b5dccae605eb7b6fdbefbf649d1c7cae1
lrmiguel <leo.rmiguel@gmail.com>
2021-03-18 15:15:14 -0300

    Initial commit

---
+++ datas.py
@@ -0,0 +1,9 @@
+class Data:
+    def __init__(self, dia, mes, ano):
+        print("Construindo o objeto Data: {}".format(self))
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def formatada(self):
+        print("{}/{}/{}".format(self.dia, self.mes, self.ano))
